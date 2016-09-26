4c4648f7ba56e5dd85ff03fce89143124f5e71a0
yaelAlush <alush.yael@gmail.com>
2016-09-26 15:30:42 +0300

    Add new activity

--- client/ActivityToCalories/ActivityToCalories.js
+++ client/ActivityToCalories/ActivityToCalories.js
@@ -1,5 +1,6 @@
 var mainUrl="http://34a6e014.ngrok.io/CaloriesCalculator.svc/";
 GetAllActivities();
+SubmitActivityModal();
 
 function GetAllActivities()
 {
@@ -10,3 +11,21 @@ function GetAllActivities()
         });
     });
 }
+
+function SubmitActivityModal()
+{
+    $("#activity-form").submit(function(e){
+        e.preventDefault();
+        var activityType=$("#activity-list option:selected");
+        var activityTime=$("#activity-time").val();
+
+        $.getJSON(mainUrl+"GetBurnedCalories?id="+activityType.val()+"&trainingMinutes="+activityTime, function(data) {
+            $("#choosed-activity").append("<div class='list-group-item'> <span class='glyphicon glyphicon-remove btn'></span>" + activityType.text() + ": " + activityTime+ "</div>");
+
+            app.outtakeCalories+=data;
+        });
+
+        $('#sportModal').modal('toggle');
+    })
+}
+
