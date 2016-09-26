f41e3a402d6104b155231282ba67621aea46f699
romhaviv <romhaviv@gmail.com>
2016-09-26 18:31:40 +0300

    slider affects stuff

--- client/common/helpers/helper-methods.js
+++ client/common/helpers/helper-methods.js
@@ -11,22 +11,30 @@ function getBmr(kg, fat) {
     return kg * (1 -(fat / 100)) * 33.5;
 }
 function getDailyCaloriesBalance(bmr){
-    return (app.intakeCalories - app.outtakeCalories) - bmr;
+    return (app.intake.calories - app.outtakeCalories) - bmr;
 }
 function updateToDay(day){
-    var ckg = app.weight;
-    var cfatKg = startingKg*(app.fatPercentage/100);
+    var ckg = Number(app.weight);
+    var cfatKg = ckg*(app.fatPercentage/100);
     var maxFatCal = 69*cfatKg;
+    var dailyCals = 0;
+    var cbmr = 0;
     for(var i = 0; i < day; i++){
-        var cbmr = getBmr(ckg, cfatKg/ckg);
-        var dailyCals = getDailyCaloriesBalance(cbmr);
-        var fatKg = min(dailyCals, maxFatCal)/7700;
-        cfatKg -= fatKg;
-        if(maxFatCal > dailyCals){
-            var muscleKg = (dailyCals-maxFatCal)/1200; //sub muscle weight;
-            ckg -= muscleKg;
+        cbmr = getBmr(ckg, (cfatKg/ckg)*100);
+        dailyCals = getDailyCaloriesBalance(cbmr);
+        var fatKg = Math.max(dailyCals, -maxFatCal)/7700;
+        cfatKg += fatKg;
+        ckg += fatKg;
+        if(maxFatCal < -dailyCals){
+            var muscleKg = (dailyCals+maxFatCal)/1200; //sub muscle weight;
+            ckg += muscleKg;
         }
-        ckg -= cfatKg;
     }
-    updateWeight(ckg, cfatKg/ckg, (cfatKg/ckg<2) || (cfatKg/ckg>50), false);
+    //alert("fat is " +cfatKg);
+    updateWeight(Number(ckg.toFixed(1)), Number(((cfatKg/ckg)*100).toFixed(1)), !(((cfatKg/ckg)<0.02)), false);
+    return {
+        bmr:cbmr,
+        cfatKg:cfatKg,
+        dailyCals:dailyCals
+    }
 }
--- client/sliderTry.js
+++ client/sliderTry.js
@@ -4,14 +4,22 @@
 // With JQuery
 var slider = $('#lifeSlider');
 slider.slider({
+    ticks: [0, 100, 200, 300, 400],
+    ticks_positions: [0, 30, 60, 70, 90, 100],
+    ticks_labels: ['$0', '$100', '$200', '$300', '$400'],
+
     formatter: function(value) {
-        return 'יום: ' + value;
+        return'יום: ' + value;
     }
 });
 slider.on("slide", function(slideEvt) {
     app.daysInDiet = slideEvt.value;
+    var data = updateToDay(app.daysInDiet);
+    $("#lbl_bmr").text(data.bmr.toFixed(2));
+    $("#lbl_fatw").text(data.cfatKg.toFixed(2));
+    $("#lbl_daily_cals").text(data.dailyCals.toFixed(2));
     //getBmr(app.weight, app.fatPercentage);
-    updateToDay(app.daysInDiet);
+
 
     //app.fatPercentage
     //updateWeight(slideEvt.value, slideEvt.value, slideEvt.value < 50, 1);
