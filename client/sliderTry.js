846868dd20f2b54e4ac2e58bc2c47f402d18b910
romhaviv <romhaviv@gmail.com>
2016-09-26 12:18:39 +0300

    slider changes

--- client/sliderTry.js
+++ client/sliderTry.js
@@ -2,8 +2,13 @@
  * Created by rompro on 26/09/2016.
  */
 // With JQuery
-$('#ex1').slider({
+$('#lifeSlider').slider({
     formatter: function(value) {
         return 'Current value: ' + value;
     }
+
+});
+$("#lifeSlider").on("slide", function(slideEvt) {
+    app.daysInDiet = slideEvt.value;
+    updateWeight();
 });
