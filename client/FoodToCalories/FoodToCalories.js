111209cf302074056b6771d75554ef06d677ab18
slorlior <slor.lior@gmail.com>
2016-09-26 15:52:11 +0300

    Merge remote-tracking branch 'remotes/origin/master' into YaelAndLiorBranch
    # Conflicts:
    #	client/fit-fat.html

--- client/FoodToCalories/FoodToCalories.js
+++ client/FoodToCalories/FoodToCalories.js
@@ -3,6 +3,8 @@
  */
 $(document).ready(function() {
     var foodList = [];
+    var foodIndex = 0;
+
     $(".openAddFood").click(function() {
         $(".foodName").val("");
         $(".foodQuantity").val("");
@@ -15,14 +17,70 @@ $(document).ready(function() {
             var foodUnit = data.find(function(entry){
                 return entry.Key == "unit";
             }).Value
-            $(".foodList").append("<div class='list-group-item'> <span class='glyphicon glyphicon-remove btn removeFood'></span>" + foodName + " <span class='foodUnit'> " + foodQuantity + " " + foodUnit + "</span></div>");
+            var foodId = "food" + foodIndex;
+            $(".foodList").append("<div class='list-group-item' id='" + foodId + "'> <span class='glyphicon glyphicon-remove btn removeFood'></span>" + foodName + " <span class='foodUnit'> " + foodQuantity + " " + foodUnit + "</span></div>");
+            foodIndex++;
             $(".removeFood").click(function(event) {
-                var object = event.target.parentElement.remove();
+                var food = event.target.parentElement;
+                var foodIndex = food.id;
+                var foodToBeRemoved = foodList.find(function (food) {
+                    return food.id == foodIndex;
+                });
+                var data = foodToBeRemoved.data;
+                app.intake.calories -= (data.find(function(entry){
+                    return entry.Key == "calories";
+                }).Value * foodQuantity);
+                app.intake.totalFat -= (data.find(function(entry){
+                    return entry.Key == "fat";
+                }).Value * foodQuantity);
+                app.intake.cholesterol -= (data.find(function(entry){
+                    return entry.Key == "cholesterol";
+                }).Value * foodQuantity);
+                app.intake.sugars -= (data.find(function(entry){
+                    return entry.Key == "sugars";
+                }).Value * foodQuantity);
+                app.intake.protein -= (data.find(function(entry){
+                    return entry.Key == "protein";
+                }).Value * foodQuantity);
+                app.intake.sodium -= (data.find(function(entry){
+                    return entry.Key == "sodium";
+                }).Value * foodQuantity);
+                app.intake.iron -= (data.find(function(entry){
+                    return entry.Key == "iron";
+                }).Value * foodQuantity);
+
+                var foodIndexToBeRemoved = foodList.indexOf(foodToBeRemoved);
+                foodList.splice(foodIndexToBeRemoved, 1);
+                food.remove();
             });
-            app.intakeCalories += (data.find(function(entry){
+            app.intake.calories += (data.find(function(entry){
                 return entry.Key == "calories";
             }).Value * foodQuantity);
-            foodList.append(data);
+            app.intake.totalFat += (data.find(function(entry){
+                return entry.Key == "fat";
+            }).Value * foodQuantity);
+            app.intake.cholesterol += (data.find(function(entry){
+                return entry.Key == "cholesterol";
+            }).Value * foodQuantity);
+            app.intake.sugars += (data.find(function(entry){
+                return entry.Key == "sugars";
+            }).Value * foodQuantity);
+            app.intake.protein += (data.find(function(entry){
+                return entry.Key == "protein";
+            }).Value * foodQuantity);
+            app.intake.sodium += (data.find(function(entry){
+                return entry.Key == "sodium";
+            }).Value * foodQuantity);
+            app.intake.iron += (data.find(function(entry){
+                return entry.Key == "iron";
+            }).Value * foodQuantity);
+
+            var food = {
+                "id": foodId,
+                "data": data
+            };
+            foodList.push(food);
+            $("#foodModal").modal("toggle");
         });
     });
 
