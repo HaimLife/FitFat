3b456f59b022a086454242d763dfac071a339d0f
HaimH <Houri.Haim@gmail.com>
2016-09-26 14:46:58 +0300

    Added updateWeight function

--- client/setup/setup.js
+++ client/setup/setup.js
@@ -16,3 +16,54 @@ $(function(){
         $select.append($('<option></option>').val(i).html(i + " CM"));
     }
 });
+function updateWeight(kg, fat, isAlive, isSick) {
+    var fatDudeImgElement = $("#fatDudeImg");
+    if(!isAlive){
+        fatDudeImgElement.attr('src', "resources\\skeleton.png");
+    }
+    else if(app.sex === 'male'){
+        if(fat < 12){
+            fatDudeImgElement.attr('src', "resources\\fats\\m8.png");
+        }
+        else if(fat < 15){
+            fatDudeImgElement.attr('src', "resources\\fats\\m12.png");
+        }
+        else if(fat < 20){
+            fatDudeImgElement.attr('src', "resources\\fats\\m15.png");
+        }
+        else if(fat < 25){
+            fatDudeImgElement.attr('src', "resources\\fats\\m20.png");
+        }
+        else if(fat < 30){
+            fatDudeImgElement.attr('src', "resources\\fats\\m25.png");
+        }
+        else if(fat < 35){
+            fatDudeImgElement.attr('src', "resources\\fats\\m30.png");
+        }
+        else{
+            fatDudeImgElement.attr('src', "resources\\fats\\m35.png");
+        }
+    }
+    else {
+        if (fat < 20) {
+            fatDudeImgElement.attr('src', "resources\\fats\\f15.png");
+        }
+        else if (fat < 25) {
+            fatDudeImgElement.attr('src', "resources\\fats\\f20.png");
+        }
+        else if (fat < 30) {
+            fatDudeImgElement.attr('src', "resources\\fats\\f25.png");
+        }
+        else if (fat < 35) {
+            fatDudeImgElement.attr('src', "resources\\fats\\f30.png");
+        }
+        else if (fat < 40) {
+            fatDudeImgElement.attr('src', "resources\\fats\\f40.png");
+        }
+        else {
+            fatDudeImgElement.attr('src', "resources\\fats\\f45.png");
+        }
+    }
+    $(".kg").text(kg);
+    $(".fat").text(fat);
+}
