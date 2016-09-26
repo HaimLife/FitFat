ee62ee311b6cab1ec4fd2c7b8fbb858a569e16a3
slorlior <slor.lior@gmail.com>
2016-09-26 12:21:28 +0300

    Merge remote-tracking branch 'remotes/origin/master' into YaelAndLiorBranch
    # Conflicts:
    #	client/fit-fat.html

--- client/FoodToCalories/FoodToCalories.js
+++ client/FoodToCalories/FoodToCalories.js
@@ -1,7 +1,7 @@
 /**
  * Created by liorslor on 9/26/2016.
  */
-$( document ).ready(function() {
+$(document).ready(function() {
     var foodList = [];
     $(".openAddFood").click(function() {
         $(".foodName").val("");
@@ -9,10 +9,14 @@ $( document ).ready(function() {
     });
 
     $(".addFood").click(function() {
-        var food = $(".foodName").val();
-        $.getJSON("http://localhost:1044/FoodToCalories.svc/GetCalories?food="+food, function(data) {
+        var foodName = $(".foodName").val();
+        var foodQuantity = $(".foodQuantity").val();
+        $.getJSON("http://localhost:1044/FoodToCalories.svc/GetCalories?food=" + foodName, function(data) {
+            $(".foodList").append("<div class='list-group-item'> <span class='glyphicon glyphicon-remove btn'></span>" + foodName + " - " + foodQuantity + "</div>");
+            app.intakeCalories += (data.find(function(entry){
+                return entry.Key == "calories";
+            }).Value * foodQuantity);
             foodList.append(data);
-
         });
     });
 });
